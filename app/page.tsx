cc3e4d50b31c62604f5f985f880fad5938bd8c22
Ɓʘɓƴ <shboby71@gmail.com>
2024-07-29 23:37:46 +0530

    third new

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
-      <p className="text-3xl font-medium text-sky-700">
-        This is welcome message
-      </p>
-      <Button variant="destructive">Button</Button>
+      <Button>Hello sire</Button>
     </>
   );
 }
